58384fd9e5ed9109d7ad40a75ed593a85b395087
RichardHerz <rich@reactorlab.net>
2020-04-20 12:05:18 -0700

    work on new XOR example with details of initial training steps

--- MATLAB_files/simple/simple_neural.m
+++ MATLAB_files/simple/simple_neural.m
@@ -20,35 +20,38 @@ weight                W{1}         W{2}
 
 % >>>> THERE ARE SEVERAL CODE SECTIONS BELOW <<<<<<<
 
-clear all
-clc
-
 fprintf('------------ run separator ------------ \n')
+clear clc
+clear all 
 
-numInputNodes = 2;
+numInputNodes = 1;
 numOutputNodes = 1;
-numHiddenNodes = 3; % nodes per hidden layer, must be >= 1
+numHiddenNodes = 2; % nodes per hidden layer, must be >= 1
 numHiddenLayers = 1;
 
 % Learning rate alpha 
-alpha = 0.5
+alpha = 0.01;
 % Lambda is for regularization 
-lambda = 0.0
+lambda = 0.001;
 % Num of iterations 
-numepochs = 2;
+numepochs = 1e5;
 % size of batches of inputs to train at same time
-batchsize = 1; % e.g., 4 for 4 random examples per batch
+batchsize = 2; % e.g., 4 for 4 random examples per batch
+
+%% Training 
 
 % training inputs
-train_x = [1 0];
+train_x = [1
+           0];
 % put examples in columns
 train_x = train_x';
 
 % number of batches
-numbatches = 1; % need integer so floor
+numbatches = floor( size(train_x, 2) / batchsize ); % need integer so floor
            
 % training outputs as array
-train_y = [1];
+train_y = [0
+           1];
 % put examples in columns
 train_y = train_y';
 
@@ -62,20 +65,11 @@ for j = 2:numHiddenLayers+1
 end
 a{numHiddenLayers + 2} = ao;
 
-% >>>> SPECIAL <<<<<<<
-
-% % initialize weights for numHiddenLayers & numHiddenNodes 
-% % randomly in range -1 to +1
-% wi = 2*rand(numHiddenNodes,numInputNodes) - 1;
-% wh = 2*rand(numHiddenNodes,numHiddenNodes) - 1;
-% wo = 2*rand(numOutputNodes,numHiddenNodes) - 1;
-
-% >>>> SPECIAL <<<<<<<
-
-wi = [-0.174631930303051  -0.230331066278993
-   0.939106662693414   0.511878531860625
-   0.232138796450696  -0.019806025322840];
-wo = [-0.276847416651616   0.188384539901641  -0.582809141323093];
+% initialize weights for numHiddenLayers & numHiddenNodes 
+% randomly in range -1 to +1
+wi = 2*rand(numHiddenNodes,numInputNodes) - 1;
+wh = 2*rand(numHiddenNodes,numHiddenNodes) - 1;
+wo = 2*rand(numOutputNodes,numHiddenNodes) - 1;
 
 if (numHiddenLayers > 1)
     W = {wi};
@@ -89,10 +83,7 @@ elseif (numHiddenLayers == 1)
 else
     fprintf(' xxxx this program requires at least 1 hidden layer xxxxx \n')
     return
-end
-
-W1 = W{1}
-W2 = W{2}
+end  
 
 % initialize biases
 initBias = 0;
@@ -104,23 +95,16 @@ end
 B{numHiddenLayers+1} = Bo;
 dB = B;
 
-% d = {[0] [0]}
-% dW = {[88] [99]}
-
-% dW{2}
-
 for j = 1 : numepochs
     % randomly rearrange the training data for each epoch
     % We keep the shuffled index in kk, so that the input and output could
     % be matched together
     kk = randperm(size(train_x, 2)); 
-    
-    jj = j
 
     for b = 1 : numbatches 
 
-        a{1} = train_x; 
-        y = train_y;
+        a{1} = train_x(:, kk( (b-1)*batchsize+1 : b*batchsize ) ); 
+        y = train_y(:, kk( (b-1)*batchsize+1 : b*batchsize ) );
         
         % forward propagation
         
@@ -130,11 +114,6 @@ for j = 1 : numepochs
             % with biases B
             a{i} = sigmaFunc(bsxfun( @plus, W{i-1}*a{i-1}, B{i-1} ) );
         end
-        
-        bb = b
-        a1 = a{1}
-        a2 = a{2}
-        a3 = a{3}
        
     %{
     Start Back-Propagation in order to train network 
@@ -174,8 +153,6 @@ for j = 1 : numepochs
     %}
     i = numHiddenLayers+1;
     d{i} = -(y - a{i+1}) .* a{i+1} .* (1 - a{i+1});
-    
-    d2 = d{2}
 
     %{
     moving "back" toward the input, 
@@ -187,8 +164,6 @@ for j = 1 : numepochs
     for i = numHiddenLayers : -1 : 1
         d{i} = W{i+1}' * d{i+1} .* a{i+1} .* (1 - a{i+1});
     end 
-    
-    d1 = d{1}
 
     %{
     update connection weights using the gradient descent method
@@ -200,54 +175,145 @@ for j = 1 : numepochs
     of the errors with respect to the connection weights.
     %}
     
-%     dW{2}
-%     ddWW2 = dW{2}
-    
     for i = 1 : numHiddenLayers+1
-        
-        ii = i
-%         dd = d{i}
-%         ainv = a{i}'
-        
-%         WWi = W{i}
-        
-        if i == 1
-            dW1 = d{i} * a{i}'
-            dW{1} = dW1
-        else
-%             ii = i
-%             class(dW{i}) 
-            dW2 = d{i} * a{i}'
-%             dW{2}
-%             ddWW2 = dW{2}
-            dW{2} = dW2;
-        end
-        
-%         ddWW = dW{i} 
-        
-        WW_current = W{i}
-        
+        dW{i} = d{i} * a{i}';
         % L2 regularization is used for W, which is the lambda * W term 
         W{i} = W{i} - alpha * (dW{i} - lambda * W{i}); 
-        
-        WW_new = W{i}
     end
+
+%     % update biases added to nodes in hidden layers
+%     for i = 1 : numHiddenLayers
+%         dB{i} = sum(d{i},2);
+%         B{i} = B{i} + alpha * dB{i};
+%     end
         
     end
 end
 
+% save workspace so we can use below after clearing 
+save('WS')
 
-fprintf('  DONE - NOW DO FINAL FORWARD PROP  \n')
+%% Testing
 
-        % forward propagation
-        
-        for i = 2 : numHiddenLayers + 2
-            % without biases B
-            % a{i} = sigmaFunc( W{i-1}*a{i-1} );
-            % with biases B
-            a{i} = sigmaFunc(bsxfun( @plus, W{i-1}*a{i-1}, B{i-1} ) );
-        end
+% just loading same workspace but is useful
+% for re-using the weights W
+% when only running this or following sections
+clear all
+load('WS.mat')
 
-        a1 = a{1}
-        a2 = a{2}
-        a3 = a{3}
+clc
+
+% check all training examples
+
+tsum = 0; % for count of number of errors
+
+for tn = 1:size(train_x, 2)
+    
+    a{1} = train_x(:,tn);
+    x = a{1};
+    % zzz fprintf('input: %i %i \n',x(1),x(2))
+    fprintf('input: %i %i \n',x(1))
+    for i = 2 : numHiddenLayers + 2
+        a{i} = sigmaFunc( W{i-1}*a{i-1} );
+    end
+    
+    yex = a{numHiddenLayers + 2};
+    [tMax tIex] = max(yex);
+    
+    y = train_y(:,tn);
+    [tMax tIans] = max(y);
+    
+    if round(yex) == round(y)
+        fprintf('answer is good %i \n',yex)
+    else
+        fprintf('answer is BAD %i \n\n',yex)
+        tsum = tsum + 1;
+    end
+    
+end
+    
+if tsum == 0
+    fprintf('GOOD - no errors \n')
+else
+    fprintf('BAD - %i errors found \n', tsum)
+end
+
+%% set an single input to image below 
+
+tn = 2; % uses tn several places below, which single input to use 
+
+a{1} = train_x(:,tn);
+x = a{1};
+% zzz fprintf('input: %i %i \n',x(1),x(2))
+fprintf('input: %i %i \n',x(1))
+for i = 2 : numHiddenLayers + 2
+    a{i} = sigmaFunc( W{i-1}*a{i-1} );
+end
+
+yex = a{numHiddenLayers + 2};
+[tMax tIex] = max(yex);
+
+y = train_y(:,tn);
+[tMax tIans] = max(y);
+
+fprintf('y is %g \n',y)
+fprintf('yex is %g \n',yex)
+
+if round(y) == round(yex)
+    fprintf('answer is good \n')
+else
+    fprintf('answer is BAD \n')
+    tsum = tsum + 1;
+end
+
+%% imaging node activation values (a)
+
+fprintf('----- image node values -------- \n')
+% get min and max of entire set so can image
+% on same color scale
+% for node values with sigma function, range is 0-1
+% fprintf('activation min = %g, activation max = %g \n',minim,maxim)
+tt = ["INPUT" "layer 1" "OUTPUT"];
+figure('Name','Node values', 'NumberTitle','off')
+for j = 1:numHiddenLayers+2
+    im = a{j};
+    % subplot(2,3,j), image(64*im), title(tt(j));
+    nhl = numHiddenLayers+2;
+    % subplot(2,nhl,j), image(64*im), title(tt(j));
+    subplot(2,nhl,j), image(64*im);
+end
+cm = colormap(gray(64));
+cm = flipud(cm); % change 0 to white, 1 to black
+colormap(cm);
+
+%% imaging connection weights (W)
+
+fprintf('----- image connection weights -------- \n')
+% get min and max of entire set so can image
+% on same color scale
+maxim = -99;
+minim = 99;
+for j = 2:numHiddenLayers+1
+    tmaxim = max(max(W{j}))
+    tminim = min(min(W{j}))
+    if tmaxim > maxim
+        maxim = tmaxim;
+    end
+    if tminim < minim
+        minim = tminim;
+    end
+end
+imspan = maxim-minim;
+fprintf('weight min = %g, weight max = %g \n',minim,maxim)
+rcm = 64;
+% colormap(jet(rcm));
+figure('Name','Connection weights', 'NumberTitle','off')
+for j = 1:numHiddenLayers+1
+    im = W{j};
+    im = rcm * (im - minim)/imspan;
+    im = im'; % so now row is start node, column is end node in next layer
+    subplot(1,numHiddenLayers+1,j), imagesc(im), title(sprintf('W %i ',j));
+end
+cm = colormap(gray(64));
+cm = flipud(cm); % change 0 to white, 1 to black
+colormap(cm);
